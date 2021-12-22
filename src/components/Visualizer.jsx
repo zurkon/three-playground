4af892cb23f0895cc4c6e4bd145a05201b65b733
Zurkon <proflucas.yoshioka@gmail.com>
2021-12-21 23:07:44 -0300

    Added resize EventListener

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -87,6 +87,13 @@ const Visualizer = () => {
       composer.render();
     }
 
+    addEventListener('resize', () => {
+      camera.aspect = innerWidth / innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(innerWidth, innerHeight);
+    });
+
     ref.current.appendChild(renderer.domElement);
 
     return () => {
